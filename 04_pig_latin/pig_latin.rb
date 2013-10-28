49e80c2aa6eeeb5eefcd6b32e2d32ddd37f60c00
Graham Lavender <glavender@gmail.com>
2013-10-28 17:57:31 -0400

    Added a comment to 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,3 +1,4 @@
+#I'm particularly proud of this one. Chris helped me with a bit of refactoring, but I made it run all by myself, and the code below is 95% mine.
 def translate(word)
     arr = word.split(" ")
     arr.map do |x|
