bb7fc149d9aadca1ed66eb9dffcabf79ef68ce6c
Graham Lavender <glavender@gmail.com>
2013-10-29 15:53:35 -0400

    Refactored change maker - fewer lines of code, but an absurd number of parentheses

--- make_change/make_change.rb
+++ make_change/make_change.rb
@@ -2,14 +2,10 @@ class Changer
 
 def self.make_change(money)
   change = []
-  quarters = money / 25
-  quarters.times {change << 25}
-  dimes = (money % 25) / 10
-  dimes.times {change << 10}
-  nickels = ((money % 25) % 10) / 5
-  nickels.times {change << 5}
-  pennies = (((money % 25) % 10) % 5)
-  pennies.times {change << 1}
+  (money / 25).times {change << 25}
+  ((money % 25) / 10).times {change << 10}
+  (((money % 25) % 10) / 5).times {change << 5}
+  ((((money % 25) % 10) % 5)).times {change << 1}
   change
 end
 
