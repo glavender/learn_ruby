4679cdd15aeb708ea4d9e3786cecc03bbe93ed8f
Graham Lavender <glavender@gmail.com>
2013-10-30 18:08:41 -0400

    Refactored 04 to handle input that contains no vowels.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,9 +2,12 @@
 def translate(word)
     arr = word.split(" ")
     arr.map do |x|
-      num = x.index(/[aeiou]/)
-      num += 1 if x[num - 1] == "q"
-      x[num..-1] + x[0...num] + "ay"
+      if x.match(/[aeiouy]/) == nil
+        "invalid"
+      else
+        num = x.index(/[aeiouy]/)
+        num += 1 if x[num - 1] == "q"
+        x[num..-1] + x[0...num] + "ay"
+      end
     end.join(" ")
 end
-
